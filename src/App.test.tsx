ea1be710179cc403939d0352075cef1bdc3a4d57
Emre Kaçan <emrekacan@gmail.com>
2023-09-19 02:32:46 +0300

    TypeScript configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,5 +10,7 @@ test("renders app correctly", () => {
     </Provider>,
   );
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this can be fixed later
   expect(getByText(/Sports Betting/i)).toBeInTheDocument();
 });
